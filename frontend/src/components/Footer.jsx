cc501397672aba55b274ff89bdbdef5e942c8400
slvignesh05 <123097688+slvignesh05@users.noreply.github.com>
2025-06-27 19:23:16 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
-    <footer id="contact" className="py-10 text-center text-xs text-gray-500">
-      Built&nbsp;with&nbsp;❤️ &amp;&nbsp;yfinance • © 2025 SnapChart
+    <footer
+      id="contact"
+      className="py-10 text-center text-xs text-gray-800 bg-white shadow-inner"
+    >
+      Built&nbsp;with&nbsp;❤️ &nbsp;+&nbsp; yfinance • © 2025&nbsp;Stocks&nbsp;down&nbsp;again
+      &nbsp;(S.&nbsp;Lakshmi&nbsp;Vignesh)
     </footer>
   );
 }
